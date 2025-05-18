9917f3d9ec9a5252caefa2d5b52c1f965d0bf51b
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-05-18 13:40:58 -0400

    Command pattern  solved

--- 03-comportamiento/02-command.ts
+++ 03-comportamiento/02-command.ts
@@ -12,3 +12,118 @@
  *
  *
  */
+
+interface Command {
+  execute(): void;
+}
+
+class Light {
+  turnOn(): void {
+    console.log("Lights turned on");
+  }
+
+  turnOff(): void {
+    console.log("Lights turned off");
+  }
+}
+
+class Fan {
+  on(): void {
+    console.log("Fan turned on");
+  }
+
+  off(): void {
+    console.log("Fan turned off");
+  }
+}
+
+class LightOnCommand implements Command {
+  constructor(private light: Light) {}
+
+  execute(): void {
+    this.light.turnOn();
+  }
+}
+
+class LightOffCommand implements Command {
+  constructor(private light: Light) {}
+
+  execute(): void {
+    this.light.turnOff();
+  }
+}
+
+class FanOnCommand implements Command {
+  constructor(private fan: Fan) {}
+
+  execute(): void {
+    this.fan.on();
+  }
+}
+
+class FanOffCommand implements Command {
+  constructor(private fan: Fan) {}
+
+  execute(): void {
+    this.fan.off();
+  }
+}
+
+class RemoteControl {
+  private commands: Record<string, Command> = {};
+
+  setCommand(button: string, command: Command): void {
+    this.commands[button] = command;
+  }
+
+  pressButton(button: string): void {
+    if (this.commands[button]) {
+      this.commands[button].execute();
+      return;
+    }
+
+    console.log(`Button: ${button} does not have a command assigned`);
+  }
+}
+
+function main() {
+  const remoteControl = new RemoteControl();
+  const light = new Light();
+  const fan = new Fan();
+
+  const lightOnCommand = new LightOnCommand(light);
+  const lightOffCommand = new LightOffCommand(light);
+
+  const fanOnCommand = new FanOnCommand(fan);
+  const fanOffCommand = new FanOffCommand(fan);
+
+  remoteControl.setCommand("1", lightOnCommand);
+  remoteControl.setCommand("2", lightOffCommand);
+  remoteControl.setCommand("3", fanOnCommand);
+  remoteControl.setCommand("4", fanOffCommand);
+
+  let continueProgram = true;
+
+  do {
+    console.clear();
+    const pressedButton =
+      prompt(`Press a remote control's button
+      1. Turn on the lights 
+      2. Turn off the lights 
+      3. Turn on the fan 
+      4. Turn off the fan 
+
+      Button: 
+      `) ?? "";
+
+    remoteControl.pressButton(pressedButton);
+
+    const continueProgramResponse = prompt(
+      `\nWant to continue? (y/n)`
+    )?.toLocaleLowerCase();
+
+    continueProgram = continueProgramResponse == "y";
+  } while (continueProgram);
+}
+
+main();
