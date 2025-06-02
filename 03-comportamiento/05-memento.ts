7f45a5695e2270393938b7015253ad4c847a67af
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-06-01 21:45:04 -0400

    Memento pattern solved

--- 03-comportamiento/05-memento.ts
+++ 03-comportamiento/05-memento.ts
@@ -8,3 +8,95 @@
  *
  * https://refactoring.guru/es/design-patterns/memento
  */
+
+class GameMemento {
+  constructor(
+    private level: number,
+    private health: number,
+    private position: string
+  ) {}
+
+  getLevel(): number {
+    return this.level;
+  }
+
+  getHealth(): number {
+    return this.health;
+  }
+
+  getPosition(): string {
+    return this.getPosition();
+  }
+}
+
+class Game {
+  constructor(
+    private level: number = 1,
+    private health: number = 100,
+    private position: string = "Inicio"
+  ) {
+    console.log(`
+      Jugando en el nivel: ${level}
+      salud: ${health}
+      posición: ${position}
+      `);
+  }
+
+  save(): GameMemento {
+    return new GameMemento(this.level, this.health, this.position);
+  }
+
+  play(level: number, health: number, position: string) {
+    this.level = level;
+    this.health = health;
+    this.position = position;
+
+    console.log(`
+      Jugando en el nivel: ${level}
+      salud: ${health}
+      posición: ${position}
+      `);
+  }
+
+  restore(memento: GameMemento) {
+    this.level = memento.getLevel();
+    this.health = memento.getHealth();
+    this.position = memento.getPosition();
+
+    console.log(`
+      Restauración en el nivel: ${this.level}
+      salud: ${this.health}
+      posición: ${this.position}
+      `);
+  }
+}
+
+class GameHistory {
+  private mementos: GameMemento[] = [];
+
+  push(memento: GameMemento) {
+    this.mementos.push(memento);
+  }
+
+  pop(): GameMemento | undefined {
+    return this.mementos.pop();
+  }
+}
+
+function main() {
+  const game = new Game();
+  const history = new GameHistory();
+
+  history.push(game.save());
+
+  game.play(2, 95, "El Valle De Oz");
+  history.push(game.save());
+
+  game.play(3, 70, "Bosque Infernal");
+  history.push(game.save());
+
+  game.play(4, 50, "Castillo del Dragón");
+  game.restore(history.pop()!);
+}
+
+main();
