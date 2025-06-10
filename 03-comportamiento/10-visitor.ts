72f3f81c3f876e61ef4b2f82346517866e73610d
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-06-09 20:51:44 -0400

    Visitor pattern solved

--- 03-comportamiento/10-visitor.ts
+++ 03-comportamiento/10-visitor.ts
@@ -11,6 +11,8 @@
  * https://refactoring.guru/es/design-patterns/visitor
  */
 
+import chalk from "chalk";
+
 /**
  * Contexto: Imagina que estás diseñando un sistema para un parque
  * temático con diferentes tipos de atracciones:
@@ -24,3 +26,175 @@
  * de la atracción como del tipo de visitante,
  * sin modificar las clases originales.
  */
+
+interface Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void;
+
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void;
+
+  visitFerriesWheel(ferriesWheel: FerriesWheel): void;
+}
+
+interface Attraction {
+  accept(visitor: Visitor): void;
+  getPrice(): number;
+}
+
+class RollerCoaster implements Attraction {
+  private price: number = 50;
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  accept(visitor: Visitor): void {
+    visitor.visitRollerCoaster(this);
+  }
+}
+
+class HauntedHouse implements Attraction {
+  private price: number = 40;
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  accept(visitor: Visitor): void {
+    visitor.visitHauntedHouse(this);
+  }
+}
+
+class FerriesWheel implements Attraction {
+  private price: number = 30;
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  accept(visitor: Visitor): void {
+    visitor.visitFerriesWheel(this);
+  }
+}
+
+// Visitors
+class ChildVisitor implements Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+    console.log(
+      chalk.bold.blue(
+        `Niño en Montaña Rusa: Precio con descuento de ${
+          rollerCoaster.getPrice() * 0.5
+        }`
+      )
+    );
+  }
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+    console.log(
+      chalk.bold.blue(
+        `Niño en Casa del Terror: Precio con descuento de ${
+          hauntedHouse.getPrice() * 0.7
+        }`
+      )
+    );
+  }
+  visitFerriesWheel(ferriesWheel: FerriesWheel) {
+    console.log(
+      chalk.bold.blue(
+        `Niño en Casa del Terror: Precio con descuento de ${
+          ferriesWheel.getPrice() * 0.6
+        }`
+      )
+    );
+  }
+}
+
+class AdultVisitor implements Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+    console.log(
+      chalk.bold.blue(
+        `Adulto en Montaña Rusa: Precio con descuento de ${rollerCoaster.getPrice()}`
+      )
+    );
+  }
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+    console.log(
+      chalk.bold.blue(
+        `Adulto en Casa del Terror: Precio con descuento de ${hauntedHouse.getPrice()}`
+      )
+    );
+  }
+  visitFerriesWheel(ferriesWheel: FerriesWheel) {
+    console.log(
+      chalk.bold.blue(
+        `Adulto en Casa del Terror: Precio con descuento de ${ferriesWheel.getPrice()}`
+      )
+    );
+  }
+}
+
+class SeniorVisitor implements Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+    console.log(
+      chalk.bold.blue(
+        `Adulto Mayor en Montaña Rusa: Precio con descuento de ${
+          rollerCoaster.getPrice() * 0.85
+        }`
+      )
+    );
+  }
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+    console.log(
+      chalk.bold.blue(
+        `Adulto Mayor en Casa del Terror: Precio con descuento de ${
+          hauntedHouse.getPrice() * 0.85
+        }`
+      )
+    );
+  }
+  visitFerriesWheel(ferriesWheel: FerriesWheel) {
+    console.log(
+      chalk.bold.blue(
+        `Adulto Mayor en Casa del Terror: Precio con descuento de ${
+          ferriesWheel.getPrice() * 0.85
+        }`
+      )
+    );
+  }
+}
+
+function main() {
+  const attractions: Attraction[] = [
+    new RollerCoaster(),
+    new HauntedHouse(),
+    new FerriesWheel(),
+  ];
+
+  console.log(chalk.bold.yellow("\nPrecios Normales"));
+
+  console.log(
+    chalk.bold.gray(`Montaña Rusa: ${new RollerCoaster().getPrice()}`)
+  );
+
+  console.log(
+    chalk.bold.gray(`Casa del Terror: ${new HauntedHouse().getPrice()}`)
+  );
+
+  console.log(
+    chalk.bold.gray(`Rueda de la fortuna: ${new FerriesWheel().getPrice()}`)
+  );
+
+  console.log("\n\n");
+
+  console.log(chalk.bold.green("\nVisitante Niño"));
+
+  attractions.forEach((attraction) => attraction.accept(new ChildVisitor()));
+
+  console.log(chalk.bold.green("\nVisitante Adulto"));
+
+  attractions.forEach((attraction) => attraction.accept(new AdultVisitor()));
+
+  console.log(chalk.bold.green("\nVisitante Adulto Mayor"));
+
+  attractions.forEach((attraction) => attraction.accept(new SeniorVisitor()));
+}
+
+main();
